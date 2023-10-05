b5493045e29f37ddc8f0ca4da4e67ac07596c823
William-Carbonneau <123512852+William-Carbonneau@users.noreply.github.com>
2023-10-05 09:41:37 -0400

    Added driver and test scene

---
+++ src/main/java/edu/vanier/waveSim/controllers/SimDriverController.java
@@ -0,0 +1,20 @@
+package edu.vanier.waveSim.controllers;
+
+import javafx.fxml.FXML;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Controller class of the MainApp's UI.
+ *
+ * @author frostybee
+ */
+public class SimDriverController {
+
+    private final static Logger logger = LoggerFactory.getLogger(MainAppController.class);
+
+    @FXML
+    public void initialize() {
+        
+    }
+}
--- src/main/java/edu/vanier/waveSim/tests/Driver.java
+++ src/main/java/edu/vanier/waveSim/tests/Driver.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package edu.vanier.waveSim.tests;
 
+import edu.vanier.waveSim.controllers.MainAppController;
+import edu.vanier.waveSim.controllers.SimDriverController;
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- *
- * @author 2265724
+ * TODO document this
  */
-public class Driver {
-    
+public class Driver extends Application {
+
+    private final static Logger logger = LoggerFactory.getLogger(Driver.class);
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            logger.info("Bootstrapping the application...");
+            //-- 1) Load the scene graph from the specified FXML file and 
+            // associate it with its FXML controller.
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/BasicCanvasTest.fxml"));
+            loader.setController(new SimDriverController());
+            Pane root = loader.load();
+            //-- 2) Create and set the scene to the stage.
+            Scene scene = new Scene(root, 500, 300);
+            primaryStage.setScene(scene);
+            primaryStage.sizeToScene();
+            primaryStage.setAlwaysOnTop(true);
+            primaryStage.show();
+        } catch (IOException ex) {
+            logger.error(ex.getMessage(), ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
