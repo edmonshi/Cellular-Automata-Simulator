5c3b2f9442204a088ad1398caf3aadebe2abf28c
William Carbonneau <56795560+WMCarbonneau@users.noreply.github.com>
2023-10-09 23:49:37 -0400

    Create SimLogic.java

---
+++ src/main/java/edu/vanier/waveSim/models/SimLogic.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.vanier.waveSim.models;
+
+/**
+ *
+ * @author William Carbonneau <2265724 at edu.vaniercollege.ca>
+ */
+public class SimLogic {
+    int[][] current;
+    int[][] nextFrame;
+    
+    
+    
+    
+}
