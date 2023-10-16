67a5b657b8dbaeaea914562d18de18768831e539
William-Carbonneau <123512852+William-Carbonneau@users.noreply.github.com>
2023-10-16 14:03:26 -0400

    Update ConwayGameOfLifeLogic.java

--- src/main/java/edu/vanier/waveSim/models/ConwayGameOfLifeLogic.java
+++ src/main/java/edu/vanier/waveSim/models/ConwayGameOfLifeLogic.java
@@ -5,15 +5,26 @@
 package edu.vanier.waveSim.models;
 
 import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author 2265724
  */
 public class ConwayGameOfLifeLogic extends CellularLogic{
+    
+    private final static Logger logger = LoggerFactory.getLogger(SimLogicWave1.class);
 
     public ConwayGameOfLifeLogic(Canvas operatingCanvas, int widthX, int heightY) {
         super(operatingCanvas, widthX, heightY);
+        // deal with scaling
+        if (scaling < 1 || scaling % 2 != 0) {
+            logger.error("scaling is wrong, setting to 1 by default");
+        }else {
+            setScaling(scaling);
+        }
     }
 
     @Override
