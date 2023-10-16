62783e9e74db76650ea0bba01906d70ee9c9c764
William-Carbonneau <123512852+William-Carbonneau@users.noreply.github.com>
2023-10-16 13:53:47 -0400

    Create ConwayGameOfLifeLogic.java

---
+++ src/main/java/edu/vanier/waveSim/models/ConwayGameOfLifeLogic.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.vanier.waveSim.models;
+
+/**
+ *
+ * @author 2265724
+ */
+public class ConwayGameOfLifeLogic extends CellularLogic{
+
+    @Override
+    public void simFrame() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    
+}
